f6751765f5014da84c7d742a8a3eaae87e213f2a
Przemo199 <przemo199@onet.pl>
2021-01-04 20:52:53 +0000

    Added start and stop functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-function updateYear() {
-  document.getElementById("year").textContent = new Date().getFullYear();
+"use strict";
+
+var interval;
+
+function startUpdates() {
+	updateValues();
+	interval = window.setInterval(updateValues, 100);
 }
 
-function updateHours() {
-  document.getElementById("hours").textContent = new Date().getHours();
+function stopUpdates() {
+	window.clearInterval(interval);
+}
+
+function updateValues() {
+	var date = new Date();
+	document.getElementById("year").textContent = date.getFullYear();
+	document.getElementById("month").textContent = date.getMonth();
+	document.getElementById("hour").textContent = date.getHours();
+	document.getElementById("minute").textContent = date.getMinutes();
+	document.getElementById("second").textContent = date.getSeconds()
+	document.getElementById("millisecond").textContent = date.getMilliseconds();
+}
+
+function addEventListeners() {
+	document.getElementById("startButton").onclick = startUpdates;
+	document.getElementById("stopButton").onclick = stopUpdates;
 }
