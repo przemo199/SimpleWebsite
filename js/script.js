c266ad7b84d4ae3655caea2987e0537939fa4e58
Przemo199 <przemo199@onet.pl>
2021-01-05 22:10:26 +0000

    Cleaned up code style

--- js/script.js
+++ js/script.js
@@ -15,20 +15,20 @@ function stopUpdates() {
 }
 
 function updateValues() {
-	var date = new Date();
-	document.getElementById("year").textContent = date.getFullYear();
-	document.getElementById("month").textContent = date.getMonth();
-	document.getElementById("date").textContent = date.getDate();
-	document.getElementById("day").textContent = date.getDay();
-	document.getElementById("hour").textContent = date.getHours();
-	document.getElementById("minute").textContent = date.getMinutes();
-	document.getElementById("second").textContent = date.getSeconds()
-	document.getElementById("millisecond").textContent = date.getMilliseconds();
+  var date = new Date();
+  document.getElementById("year").textContent = date.getFullYear();
+  document.getElementById("month").textContent = date.getMonth();
+  document.getElementById("date").textContent = date.getDate();
+  document.getElementById("day").textContent = date.getDay();
+  document.getElementById("hour").textContent = date.getHours();
+  document.getElementById("minute").textContent = date.getMinutes();
+  document.getElementById("second").textContent = date.getSeconds()
+  document.getElementById("millisecond").textContent = date.getMilliseconds();
 }
 
 function addEventListeners() {
   document.getElementById("startButton").onclick = startUpdates;
-	document.getElementById("stopButton").onclick = stopUpdates;
+  document.getElementById("stopButton").onclick = stopUpdates;
 }
 
 addEventListeners();
