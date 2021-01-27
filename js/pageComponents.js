672138bdabfc7ec65e1cdd6e87e329a7291fdcd5
Przemo199 <przemo199@onet.pl>
2021-01-27 06:11:05 +0000

    More user details added

--- js/pageComponents.js
+++ js/pageComponents.js
@@ -1,1 +1,3 @@
+"use strict";
+
 $("#nav_container").load("common/navbar.html");
--- js/userdetailsScripts.js
+++ js/userdetailsScripts.js
@@ -1,9 +1,71 @@
+"use strict";
+
 $("#user_agent").text(navigator.userAgent);
+
 $("#connection").text(navigator.connection.effectiveType);
-for (var property in navigator) {
-  console.log(property)
-  console.log(navigator[property])
-}
+
+// for (var property in navigator) {
+//   console.log(property)
+//   console.log(navigator[property])
+// }
+
 navigator.getBattery().then((battery) => {
-  $("#battery").text(battery)
-})
+  $("#battery").text(battery.level*100 + '%');
+});
+
+$.get('https://www.cloudflare.com/cdn-cgi/trace', function(response) {
+  let data = response.split("\n")
+  $("#ip").text(data[2].substring(3));
+  $("#location").text(data[8].substring(4));
+});
+
+$("#webdriver").text(navigator.webdriver ? navigator.webdriver : "undefined");
+
+/**
+ * Get the user IP throught the webkitRTCPeerConnection
+ * @param onNewIP {Function} listener function to expose the IP locally
+ * @return undefined
+ */
+function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+  //compatibility for firefox and chrome
+  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+  var pc = new myPeerConnection({
+        iceServers: []
+      }),
+      noop = function() {},
+      localIPs = {},
+      ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+      key;
+
+  function iterateIP(ip) {
+    if (!localIPs[ip]) onNewIP(ip);
+    localIPs[ip] = true;
+  }
+
+  //create a bogus data channel
+  pc.createDataChannel("");
+
+  // create offer and set local description
+  pc.createOffer().then(function(sdp) {
+    sdp.sdp.split('\n').forEach(function(line) {
+      if (line.indexOf('candidate') < 0) return;
+      line.match(ipRegex).forEach(iterateIP);
+    });
+
+    pc.setLocalDescription(sdp, noop, noop);
+  }).catch(function(reason) {
+    alert("Error:" + reason);
+  });
+
+  //listen for candidate events
+  pc.onicecandidate = function(ice) {
+    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+    ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+  };
+}
+
+// Usage
+
+getUserIP(function(ip) {
+  alert("Got IP! :" + ip);
+});
