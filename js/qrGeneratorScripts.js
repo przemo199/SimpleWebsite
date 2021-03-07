f8dec5796ae29175efdf178f5b85c27630eb4ab7
Przemo199 <przemo199@onet.pl>
2021-03-07 00:00:03 +0000

    Added QR code form to fetch QR codes from google API

---
+++ js/qrGeneratorScripts.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const url = "https://chart.googleapis.com/chart?cht=qr&";
+
+function updateSrc() {
+  let query = url;
+  query +="chs=";
+  query += $("#width").val();
+  query += "x";
+  query += $("#height").val();
+  query +="&chl=";
+  query += $("#text").val();
+  query +="&choe=";
+  query += $("#encoding").val();
+  query +="&chld=";
+  query += $("#error-correction").val();
+  query +="|";
+  query += $("#margin").val();
+  console.log(query);
+  $("#qr-code").attr("src", query);
+  return false;
+}
+
+$("#generate-button").click(updateSrc);
